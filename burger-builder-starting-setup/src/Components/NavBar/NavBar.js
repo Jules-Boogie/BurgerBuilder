111f7dec95172f9e5ba2f24e5d9570ce96ae3a66
Juliet George <georgejuliet11@gmail.com>
2020-06-24 14:07:53 -0700

    app before switching to redux to manage state

--- burger-builder-starting-setup/src/Components/NavBar/NavBar.js
+++ burger-builder-starting-setup/src/Components/NavBar/NavBar.js
@@ -5,12 +5,12 @@ function navigationBar() {
     return (
 
         <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">
+                <Navbar.Brand href="/">
                     <img src="https://cdn1.vectorstock.com/i/1000x1000/02/20/breakfast-with-egg-and-toast-vector-21370220.jpg" width="30" height="30"></img>
                     Juliet's BLT</Navbar.Brand>
                 <Nav className="ml-auto">
-                    <Nav.Link  href="#home">Menu</Nav.Link>
-                    <Nav.Link  href="/">Burger Builder</Nav.Link>
+                    <Nav.Link  href="/">Menu</Nav.Link>
+                    <Nav.Link  href="/">BLT Builder</Nav.Link>
                     <Nav.Link  href="/checkout">Checkout</Nav.Link>
                     <Nav.Link  href="/orders"> My Orders</Nav.Link>
                 </Nav>
